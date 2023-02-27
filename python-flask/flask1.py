e24616f386e25085fed4988fd01bd86e7c8bf733
surya-full <surya.venu@anywhere.co>
2023-02-27 10:51:49 +0530

    updated flask basic code

--- python-flask/flask1.py
+++ python-flask/flask1.py
@@ -2,14 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "This is Home page <h1>HELLO!</h1>"
 
 
-@app.route("/<temp>")
-def user(temp):
-    return "<h1>'Hello!'</h1> <p>{}<p>".format(temp)
+@app.route("/<name>")
+def user(name):
+    return f"<h1>'Hello!'</h1> <p>{name}<p>"
+
 
 if __name__ == "__main__":
     app.run()
