df48fa12a5155c30467cb48be8c5b25364f8ba8e
surya-full <surya.venu@anywhere.co>
2023-03-10 18:35:03 +0530

    The tunetracker is now producing the requird json results

--- TuneTracker/main.py
+++ TuneTracker/main.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
-import json
+import logging
 
 app = Flask(__name__, template_folder="templates")
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        query = request.form.get('query')
+    return render_template('tune.html')
+
+
+@app.route('/spotify/search')
+def home():
+    try:
+        query = request.args.get('query')
         url = "https://spotify81.p.rapidapi.com/search"
 
         querystring = {"q": query, "type": "multi", "offset": "0", "limit": "10", "numberOfTopResults": "5"}
@@ -18,10 +24,13 @@ def index():
             "X-RapidAPI-Host": "spotify81.p.rapidapi.com"
         }
         response = requests.request("GET", url, headers=headers, params=querystring)
-        data = json.loads(response.text)
-        return render_template('tune.html', data=data)
-    else:
-        return render_template('tune.html')
+        response.raise_for_status()
+        data = response.json()
+        return jsonify(data)
+
+    except requests.exceptions.HTTPError as err:
+        logging.error(f'HTTP error occurred: {err}')
+        return jsonify({'error': str(err)})
 
 
 if __name__ == '__main__':
