838758f27f409f33381f2e83246bd3772e557190
surya-full <surya.venu@anywhere.co>
2023-03-20 11:21:27 +0530

    Modified the melody mantra

--- Melody_Mantra/static/script.js
+++ Melody_Mantra/static/script.js
@@ -8,11 +8,7 @@
          if (xhr.readyState === xhr.DONE && xhr.status === 200) {
          var results = JSON.parse(xhr.responseText);
            if (results.success) {
-<<<<<<< HEAD
-                newListHTML = results.data.map(item => `<li>${item}</li>`)
-=======
                 newListHTML = results.data.map((item, image) => `<li>${item} ==>> <img src=${results.image[image]}></li>`);
->>>>>>> Experimental
                 resultList.innerHTML = newListHTML.join('');
            }
            else{
